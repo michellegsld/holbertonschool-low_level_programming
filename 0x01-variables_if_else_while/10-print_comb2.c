26d64f095e7706dae0fca2a30f03f364d44c5d3d
Michelle Giraldo <michellegsld@gmail.com>
2019-09-19 13:59:15 -0400

    Initial. Has two for-loops and an if statement. Two variables

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,2 +1,31 @@
 #include <stdio.h>
-#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int i;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			putchar(n + '0');
+			putchar(i + '0');
+			if (!((n == 9) && (i == 9)))
+			{
+				putchar(',');
+			}
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
