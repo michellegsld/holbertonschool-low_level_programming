d0fc7bce364c942383c081b39b67cca1ee3a3ef8
Michelle Giraldo <michellegsld@gmail.com>
2019-09-19 13:10:52 -0400

    Initial. Used putchar(), two variables and 2 for-loops

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
-#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+	char l = 'a';
+
+	for (; n < 10; n++)
+		putchar(n + '0');
+	for (; l <= 'f'; l++)
+		putchar(l);
+
+	putchar('\n');
+
+	return (0);
+}
