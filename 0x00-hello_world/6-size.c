586b643b9cce5f26f4eda2ffa2065ad98c123068
Michelle Giraldo <michellegsld@gmail.com>
2019-09-18 20:13:43 -0400

    wrote initial code, used printf() and sizeof()

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("Size of a char: ", sizeof(char), " byte(s)");
+	printf("Size of an int: ", sizeof(int), " byte(s)");
+	printf("Size of a long int ", sizeof(long), " byte(s)");
+	printf("Size of a long long int ", sizeof(long long), " byte(s)");
+	printf("Size of a float", sizeof(float), " byte(s)");
+
+	return (0);
+}
