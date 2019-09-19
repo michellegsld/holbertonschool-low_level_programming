68f7db830aeaddf940bf3d78b81fc03ae0742afd
Michelle Giraldo <michellegsld@gmail.com>
2019-09-19 13:17:56 -0400

    Initial. Used for loop with if statement inside, only one variable

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
-#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (; n < 10; n++)
+	{
+		putchar(n + '0');
+		if (n < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
