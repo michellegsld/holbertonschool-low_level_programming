f964117d87c36d0a44c21ae8e86c6f0548e8c3b1
Michelle Giraldo <michellegsld@gmail.com>
2019-09-19 10:22:52 -0400

    initial. used %d, created own int, used if else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,2 +1,31 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int l;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+
+	printf("Last digit of %d is %d ", n, l);
+
+	if (l > 5)
+		printf("and is greater than 5\n");
+	else if (l == 0)
+		printf("and is 0\n");
+	else if ((l < 6) && !(l == 0))
+		printf("and is less than 6 and not 0\n");
+
+	return (0);
+}
