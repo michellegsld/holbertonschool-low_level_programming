793945623aad05b125e68862675d5df2f4ea3c68
Michelle Giraldo <michellegsld@gmail.com>
2019-09-19 11:21:13 -0400

    Initial. Similar to #2, just changed to > and started at z, as well as did l--

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
-#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l = 'z';
+
+	for (; l >= 'a'; l--)
+		putchar(l);
+
+	putchar('\n');
+
+	return (0);
+}
