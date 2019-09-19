986314e1f4f4581a467a4807f01c57b26f0440e4
Michelle Giraldo <michellegsld@gmail.com>
2019-09-19 10:44:37 -0400

    Initial. When l is initialized, is set to nothing. Added 2nd for-loop. Only used putchar()

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
-#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+
+	for (l = 'A'; l <= 'Z'; l++)
+		putchar(l);
+
+	putchar('\n');
+
+	return (0);
+}
