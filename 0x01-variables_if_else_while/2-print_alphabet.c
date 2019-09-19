66fda476c2baf42e6305372b988ebd2d74431bff
Michelle Giraldo <michellegsld@gmail.com>
2019-09-19 10:34:38 -0400

    initial. Used putchar(), created variable 'l', prints a-z

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
-#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l = 'a';
+
+	for (; l <= 'z'; l++;)
+		putchar(l);
+
+	putchar("\n");
+
+	return (0);
+}
