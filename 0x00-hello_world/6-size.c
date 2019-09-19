e529c0f3a73c41adb53ca1d956de9950f3267052
Michelle Giraldo <michellegsld@gmail.com>
2019-09-18 20:15:30 -0400

    forgot a whitespace, also inserted int after single long

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	printf("Size of a char: ", sizeof(char), " byte(s)");
 	printf("Size of an int: ", sizeof(int), " byte(s)");
-	printf("Size of a long int ", sizeof(long), " byte(s)");
+	printf("Size of a long int ", sizeof(long int), " byte(s)");
 	printf("Size of a long long int ", sizeof(long long), " byte(s)");
-	printf("Size of a float", sizeof(float), " byte(s)");
+	printf("Size of a float ", sizeof(float), " byte(s)");
 
 	return (0);
 }
