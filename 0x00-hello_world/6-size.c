9f74d77d8fda725a0609059fcf2dbd8f61db697d
Michelle Giraldo <michellegsld@gmail.com>
2019-09-18 20:33:53 -0400

    changed order again to remove the %ld

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %ld byte(s)\n", sizeof(char));
-	printf("Size of an int: %ld byte(s)\n", sizeof(int));
-	printf("Size of a long int %ld byte(s)\n", sizeof(long int));
-	printf("Size of a long long int %ld byte(s)\n", sizeof(long long));
-	printf("Size of a float %ld byte(s)\n", sizeof(float));
+	printf("Size of a char: ", sizeof(char), " byte(s)\n");
+	printf("Size of an int: ", sizeof(int), " byte(s)\n");
+	printf("Size of a long int: ", sizeof(long int), " byte(s)\n");
+	printf("Size of a long long int: ", sizeof(long long), " byte(s)\n");
+	printf("Size of a float: ", sizeof(float), " byte(s)\n");
 
 	return (0);
 }
