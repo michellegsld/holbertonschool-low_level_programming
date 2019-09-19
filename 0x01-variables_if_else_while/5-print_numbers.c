72ff0678692943984a29f2785e6138606f073e0b
Michelle Giraldo <michellegsld@gmail.com>
2019-09-19 10:56:25 -0400

    Initial. Used int n, used printf() in the for-loop. Kept putchar for newline

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
-#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (; n < 10; n++)
+		printf("%d", n);
+
+	putchar('\n');
+
+	return (0);
+}
