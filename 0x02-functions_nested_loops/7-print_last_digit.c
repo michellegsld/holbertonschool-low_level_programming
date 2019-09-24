4e158c40a7c5140e01994bb7a01dd5ed656abf9d
Michelle Giraldo <michellegsld@gmail.com>
2019-09-24 11:12:51 -0400

    Changing the order, so does modulo first then absolute

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,12 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		n = (-n) % 10;
-		_putchar(n + '0');
+		n = n % 10;
+		n = -n;
 	}
 	else
-	{
 		n = n % 10;
-		_putchar(n + '0');
-	}
+	_putchar(n + '0');
 
 	return (n);
 }
