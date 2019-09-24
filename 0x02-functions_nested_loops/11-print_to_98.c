436417dc2c106228864c7f3ec2c79f9b63bc380b
Michelle Giraldo <michellegsld@gmail.com>
2019-09-24 16:46:48 -0400

    Rewriting to be a lot shorter in order to fulfill betty requirements. No longer while

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,63 +2,44 @@
 
 /**
  * print_to_98 - Prints all natural numbers from the parameter to 98.
+ * @n: Parameter's value to be printed until it equals 98.
  */
 void print_to_98(int n)
 {
 	int i = 0;
 
-	while (n > 98)
-	{
-		if (n / 100 != 0)
-			_putchar(n / 100 + '0');
-		if (n >= 100)
-		{
-			i = 1;
-			n = n - 100;
-		}
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-		
-		_putchar(',');
-		_putchar(' ');
-
-		if (i == 1)
-		{
-			i = 0;
-			n = n + 100;
-		}
-		n = n - 1;
-	}
-
-	while (n < 98)
+	for (; n != 98; )
 	{
 		if (n < 0)
 		{
 			i = 1;
 			n = -n;
 		}
-		
+
 		if (i == 1)
 			_putchar('-');
+
 		if (n / 100 != 0)
 			_putchar(n / 100 + '0');
 		if (n / 10 != 0)
-			_putchar(n / 10 + '0');
+			_putchar((n / 10) % 10 + '0');
 		_putchar(n % 10 + '0');
-		
+
 		_putchar(',');
 		_putchar(' ');
-		
+
 		if (i == 1)
 		{
 			i = 0;
 			n = -n;
 		}
-		n = n + 1;
+
+		if (n > 98)
+			n = n - 1;
+		else if (n < 98)
+			n = n + 1;
 	}
-	
 	_putchar(n / 10 + '0');
 	_putchar(n % 10 + '0');
 	_putchar('\n');
-	return;
 }
