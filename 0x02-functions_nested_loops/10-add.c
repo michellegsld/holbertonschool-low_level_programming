8e926a7b7b18199ad25a3511e04097bd4c51d047
Michelle Giraldo <michellegsld@gmail.com>
2019-09-24 10:54:41 -0400

    First uploading, adds two parameters and returns it's value

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - Recieves two parameters and adds them.
+ * @x: First parameter.
+ * @y: Second parameter.
+ *
+ * Return: Always x + y.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
