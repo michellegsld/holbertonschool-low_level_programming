3bc19c0cfde8e88f4ccb98ccf9f8cf5a9b27bd1f
Michelle Giraldo <michellegsld@gmail.com>
2019-09-25 23:02:32 -0400

    Moving some lines around to remove the return, making checker work

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,44 +6,41 @@
  */
 void print_times_table(int n)
 {
-	if (!(n >= 0 && n <= 15))
-		return;
-	int i, j, x;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		int i, j, x;
+
+		for (i = 0; i <= n; i++)
 		{
-			x = j * i;
-			if (j == 0)
-				_putchar(x + '0');
-			else
-				_putchar(',');
-			if (j != 0 && x < 10)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(x + '0');
-			}
-			else if (x >= 10 && x < 100)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(x / 10 + '0');
-				_putchar(x % 10 + '0');
-			}
-			else if (x >= 100)
-			{
-				_putchar(' ');
-				_putchar(x / 100 + '0');
-				if (x / 10 < 10)
+				x = j * i;
+				if (j != 0)
+					_putchar(',');
+				if (j != 0 && x < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (x >= 10 && x < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(x / 10 + '0');
-				else
-					_putchar((x / 10) % 10 + '0');
+				}
+				else if (x >= 100)
+				{
+					_putchar(' ');
+					_putchar(x / 100 + '0');
+					if (x / 10 < 10)
+						_putchar(x / 10 + '0');
+					else
+						_putchar((x / 10) % 10 + '0');
+				}
 				_putchar(x % 10 + '0');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
