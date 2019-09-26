0c3214daca5d89146b056f3dbba7ffe1dc69e72a
Michelle Giraldo <michellegsld@gmail.com>
2019-09-26 12:49:57 -0400

    Initial pushing, Prints all numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Prints the numbers 0 to 9, followed by a newline.
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		_putchar(num + '0');
+	_putchar('\n');
+}
