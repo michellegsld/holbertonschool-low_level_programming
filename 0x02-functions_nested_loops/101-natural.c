e525ce2d9d50be795d981d50a0e5a867bbc3faec
Michelle Giraldo <michellegsld@gmail.com>
2019-09-25 23:20:22 -0400

    Initial pushing, adding all multiples of either 3 or 5 but not if it's both

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints sum of all multiples of 3 or 5 below 1024.
+ *
+ * Adds all multiples of 3 or 5 but not if it's both.
+ * Return: Always 0.
+ */
+void main(void)
+{
+	int i = 0;
+	int sum = 0;
+
+	for (; i < 1024; i++)
+	{
+		if (i % 15 != 0)
+		{
+			if (i % 3 == 0)
+				sum = sum + i;
+			if (i % 5 == 0)
+				sum = sum + i;
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
