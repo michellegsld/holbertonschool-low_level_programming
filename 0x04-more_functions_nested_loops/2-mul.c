598592df284b5442afc88bb3992623af8ddb4d19
Michelle Giraldo <michellegsld@gmail.com>
2019-09-26 12:44:44 -0400

    Initial pushing, Multiplying two integers and returning the value.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - Multiplies two integers.
+ * @a: First integer being multiplied
+ * @b: Second integer being multiplied
+ *
+ * Return: Always a times b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
