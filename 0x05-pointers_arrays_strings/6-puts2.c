1246750775925938c417d365cfb6a719f54b4760
Michelle Giraldo <michellegsld@gmail.com>
2019-09-30 16:54:48 -0400

    Initial pushing, Prints every other character in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * puts2 - Prints every other character of a string, starts with first
+ * @str: The string to be used
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+
+	_putchar('\n');
+}
