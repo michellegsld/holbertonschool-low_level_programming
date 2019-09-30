741e8ad3995b34108566717255fcd18711835b8a
Michelle Giraldo <michellegsld@gmail.com>
2019-09-30 15:24:12 -0400

    Initial pushing, Updating value pointer is pointing to, Now integer = 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Will take a pointer and update the value it points to.
+ * @n: The pointer to an int.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
