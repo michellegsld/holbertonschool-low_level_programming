5e33440f09d2c1e7dc0234c58dadd0e167f0a85a
Michelle Giraldo <michellegsld@gmail.com>
2019-09-30 15:58:28 -0400

    Initial pushing, Returning the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Function similar to strlen, returns string length
+ * @s: The string that the length must be determined of
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	
+	return (len);
+}
