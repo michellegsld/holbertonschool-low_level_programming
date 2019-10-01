b56066ff8259bf218e3d3c635ff8c46411dc00a3
Michelle Giraldo <michellegsld@gmail.com>
2019-10-01 10:42:54 -0400

    Fourth pushing, Got a length value, Add one after for null value

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,9 +6,14 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, len = 0;
+	
+	while (str[len] != '\0')
+		len++;
+	
+	len++;
 
-	while (str[i] != '\0')
+	while (str[i] != str[len])
 	{
 		_putchar(str[i]);
 		i = i + 2;
