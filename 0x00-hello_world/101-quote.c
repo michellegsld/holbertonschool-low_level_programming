10a40096b53dcf754f4288768ed3fee7ddfbcd40
Michelle Giraldo <michellegsld@gmail.com>
2019-10-01 14:54:37 -0400

    Initial pushing, Initializing an array, Using for-loop to print each char in it

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+*/
+int main(void)
+{
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\0";
+	int i;
+
+	for (i = 0; q[i] != '\0'; i++)
+		putchar(q[i]);
+
+	putchar('\n');
+	return (1);
+}
