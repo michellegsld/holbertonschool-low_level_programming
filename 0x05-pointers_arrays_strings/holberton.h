f9f8b3c40fc546fe06c442d57025acee630a4403
Michelle Giraldo <michellegsld@gmail.com>
2019-10-01 08:41:42 -0400

    Second pushing, Removing the .h from the ifndef and define

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,6 +1,5 @@
-#ifndef holberton.h
-#define holberton.h
-
+#ifndef holberton
+#define holberton
 int _putchar(char c);
 
 void reset_to_98(int *n);
@@ -22,5 +21,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
-
 #endif
