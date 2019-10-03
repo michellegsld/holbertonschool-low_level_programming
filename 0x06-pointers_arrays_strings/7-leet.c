348f910dbbe95cbfdcb771defab51df6ee5f6c8d
Michelle Giraldo <michellegsld@gmail.com>
2019-10-03 14:56:12 -0400

    Second pushing, Removing len,

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,16 +8,13 @@
  */
 char *leet(char *s)
 {
-	int i = 0, j, len = 0;
+	int i, j;
 	char num[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 	char lett[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
-	while (s[len] != '\0')
-		len++;
-
-	for (len++; i < len; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		for (j = 0; j < 10; j++)
-		if (s[i] == lett[j])
-			s[i] = num[j];
+			if (s[i] == lett[j])
+				s[i] = num[j];
 	return (s);
 }
