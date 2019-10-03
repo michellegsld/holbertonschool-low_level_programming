ea6826acd23e5a6d9f5278d3cd917366ddaf644c
Michelle Giraldo <michellegsld@gmail.com>
2019-10-03 09:44:47 -0400

    Initial.2 pushing, Removing white space, fixing betty requirements

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 /**
  * *_strcat - Concatenates two strings, changing placement of '\0'
- * @*dest: The resulting string being added to
- * @*src: The string that will be adding to
+ * @dest: The resulting string being added to
+ * @src: The string that will be adding to
  *
  * Return: Always the pointer to resulting string, dest.
  */
@@ -16,7 +16,7 @@ char *_strcat(char *dest, char *src)
 
 	for (; src[i] != '\0'; i++)
 		dest[length + i] = src[i];
-	
+
 	dest[length + i] = '\0';
 
 	return (dest);
