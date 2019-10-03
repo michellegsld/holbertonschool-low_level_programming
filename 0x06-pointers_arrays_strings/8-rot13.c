4f7e62302dd71d25e46b75a2a59be608416269e3
Michelle Giraldo <michellegsld@gmail.com>
2019-10-03 16:01:10 -0400

    Initial pushing, Creating two arrays, Comparing the string to find which letter it is, Setting it to the second array in order to encode

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * rot13 - Encodes a string into 1337
+ * @s: The string to be encoded
+ *
+ * Return: Always the encoded string.
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char *alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *rot = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; j < 52; j++)
+			if (s[i] == alpha[j])
+				s[i] = rot[j];
+
+	return (s);
+}
