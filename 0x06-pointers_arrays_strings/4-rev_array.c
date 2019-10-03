6daacfd7ea8ccea85b37dc463a36d3fb9aa9b24b
Michelle Giraldo <michellegsld@gmail.com>
2019-10-03 13:32:56 -0400

    Initial pushing, Initializing array copy, For-loop copies array a to copy, Next for-loop reverses array a

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: The array to be reversed.
+ * @n: The amount of elements in the array.
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, j = 0;
+	int copy[n];
+
+	for (; i < n; i++)
+		copy[i] = a[i];
+	
+	for (i--; j < n; j++, i--)
+		a[j] = copy[i];
+}
