f277a02923097054dc7706b14a3c58c82d31bcdf
Michelle Giraldo <michellegsld@gmail.com>
2019-10-09 18:11:06 -0400

    Seventh pushing, Swapping functions in case of check, Changing if statements around, last return now is -1 in math funtion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,6 @@
 #include "holberton.h"
 
-/**
- * math - Does the math to find the square root without changing n
- * @num: The number that will equal the square root
- * @n: The number that's being square rooted
- *
- * Return: Always num
- */
-int math(int num, int n)
-{
-	if ((n / 2) < num)
-		return (-1);
-	else if ((num * num) != n)
-		return (math(num++, n));
-	else
-		return (num);
-}
+int math(int num, int n);
 
 /**
  * _sqrt_recursion - Returns the natural square root of a number
@@ -27,5 +12,21 @@ int _sqrt_recursion(int n)
 {
 	if (n > 0)
 		return (math(1, n));
+	return (-1);
+}
+
+/**
+ * math - Does the math to find the square root without changing n
+ * @num: The number that will equal the square root
+ * @n: The number that's being square rooted
+ *
+ * Return: Always num
+*/
+int math(int num, int n)
+{
+	if ((num * num) != n)
+		return (math(num++, n));
+	else if ((num * num) == n)
+		return (num);
 	return (-1);
 }
