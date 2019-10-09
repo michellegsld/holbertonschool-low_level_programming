ed438c2f7e3b74b69fdba85bc7878f977aba58fe
Michelle Giraldo <michellegsld@gmail.com>
2019-10-09 15:17:19 -0400

    Second pushing, Adding if-statement to check, If first string is empty and second is only *

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -23,7 +23,10 @@ int wildcmp(char *s1, char *s2)
 		if (*s1 == *s2)
 			return (wildcmp(s1 + 1, s2 + 1));
 	}
-	else if ((*s1 == '\0') && *s2 == '\0')
+	else if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else if
+		(((*s1 == '\0') && ((*s2 == '*')) && (*(s2 + 1) == '\0')))
 		return (1);
 
 	return (0);
