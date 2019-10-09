2d2d2b33cc0022bf3f4bbd4a0aa5f06573a6c048
Michelle Giraldo <michellegsld@gmail.com>
2019-10-09 18:19:27 -0400

    Ninth pushing, Making num start at 0, Fixing typo in description name

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,13 +10,13 @@ int _sqrt_math(int num, int n);
  */
 int _sqrt_recursion(int n)
 {
-	if (n > 0)
-		return (_sqrt_math(1, n));
+	if (n >= 0)
+		return (_sqrt_math(0, n));
 	return (-1);
 }
 
 /**
- * _srt_math - Does the math to find the square root without changing n
+ * _sqrt_math - Does the math to find the square root without changing n
  * @num: The number that will equal the square root
  * @n: The number that's being square rooted
  *
