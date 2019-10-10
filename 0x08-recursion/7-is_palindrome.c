2e8a48691f7144a6f42a3a1b505434617edba032
Michelle Giraldo <michellegsld@gmail.com>
2019-10-09 20:21:49 -0400

    Initial pushing, three functions, 2 of which are recursive, Palindrome_check must be able to be shorter

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,65 @@
+#include "holberton.h"
+
+int _strlen_recursion(char *s);
+
+int palindrome_check(char *s, int f, int b, int end, int flag);
+
+/**
+ * is_palindrome - Determines if a string is a palindrome
+ * @s: The string
+ *
+ * Return: Either 1 or 0.
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+	int end, flag;
+
+	if (len > 1)
+	{
+		if (len % 2 == 0)
+			flag = 0;
+		else
+			flag = 1;
+		end = (len - flag) / 2;
+		return (palindrome_check(s, 0, len - 1, end, flag));
+	}
+	return (0);
+}
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: The string length to be determined
+ *
+ * Return: Always an int, the length of s.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+
+/**
+ * palindrome_check - Moves through the string
+ * @s: The string
+ * @f: Stands for forwards, check from front
+ * @b: Stands for backwards, checks from back
+ * @end: The midpoint of the word
+ * @flag: Determines if the length was even or odd
+ *
+ * Return: Either 1 or 0.
+ */
+int palindrome_check(char *s, int f, int b, int end, int flag)
+{
+	int check = 0;
+
+	if (flag == 0)
+		check = 1;
+	if (f + check == end && b == end)
+		return (1);
+	else if (s[f] == s[b])
+		return (palindrome_check(s, f + 1, b - 1, end, flag));
+	else
+		return (0);
+}
