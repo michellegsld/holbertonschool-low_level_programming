80a93af922e9a553b648199102ace90596985639
Michelle Giraldo <michellegsld@gmail.com>
2019-10-12 18:46:22 -0400

    Initial pushing, Prints argc - 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the argc
+ * @argc: Argument count, number passed to program
+ * @argv: Argument vector, 1-dimentional array
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
