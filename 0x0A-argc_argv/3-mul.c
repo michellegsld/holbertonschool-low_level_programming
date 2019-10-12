24db15d6e8a83e6efb61448d8f1581e5b6175ee3
Michelle Giraldo <michellegsld@gmail.com>
2019-10-12 19:27:54 -0400

    Initial pushing, Using stdlib.h for the atoi function, multiplies two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers
+ * @argc: Argument count, number passed to program
+ * @argv: Argument vector, 1-dimentional array
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
