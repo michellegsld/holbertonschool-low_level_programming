6bae104a2cdbce7d62f45e4da433907dc7676eaa
Michelle Giraldo <michellegsld@gmail.com>
2019-10-15 19:25:44 -0400

    Initial pushing, using malloc and a for-loop to, create a 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * **alloc_grid - 2-dimensional array of integers
+ * @width: The width of the array 
+ * @height: The height of the array
+ *
+ * Return: Pointer to 2D array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return(NULL);
+
+	grid = malloc(height * sizeof(int *));
+
+	for (i = 0; i < height; i++)
+		grid[i] = malloc(width * sizeof(int));
+
+	if (grid == NULL)
+		return (NULL);
+	
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	
+	return (grid);
+}
