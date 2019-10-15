8dd122da54d31c219fa0ca629e6efe07e1955ee2
Michelle Giraldo <michellegsld@gmail.com>
2019-10-15 18:00:13 -0400

    Fifth pushing, Accounting if strings are NULLS, changing them to empty string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,11 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0, lenb = 0;
 	char *str;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[len1] != '\0')
 		len1++;
 
