93c72abf0357c0b5c0290b9b3e05bcdc6587be17
Michelle Giraldo <michellegsld@gmail.com>
2019-10-15 16:30:48 -0400

    Initial pushing, Very similar to previous file, using while-loop to find string length

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * *_strdup - Returns a pointer to a newly allocated space in memory
+ * @str: The string to copy
+ *
+ * Return: Pointer to new space in memory
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0, len = 0;
+	char *newstr;
+
+	while (str[len] != '\0')
+		len++;
+
+	newstr = malloc(len * sizeof(char));
+
+	if (len == 0 || newstr == NULL)
+		return (NULL);
+
+	for (; i < len; i++)
+		newstr[i] = str[i];
+
+	return (newstr);
+}
