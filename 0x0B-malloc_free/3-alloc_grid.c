084b0fddd811dbe13087bbed47f8b7e3a9283d9d
Michelle Giraldo <michellegsld@gmail.com>
2019-10-15 19:40:12 -0400

    Second pushing, accounting for if grid fails and for, if grid[i] fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,7 +2,7 @@
 
 /**
  * **alloc_grid - 2-dimensional array of integers
- * @width: The width of the array 
+ * @width: The width of the array
  * @height: The height of the array
  *
  * Return: Pointer to 2D array
@@ -13,19 +13,23 @@ int **alloc_grid(int width, int height)
 	int **grid;
 
 	if (width <= 0 || height <= 0)
-		return(NULL);
+		return (NULL);
 
-	grid = malloc(height * sizeof(int *));
-
-	for (i = 0; i < height; i++)
-		grid[i] = malloc(width * sizeof(int));
+	grid = (int **)malloc(height * sizeof(int *));
 
 	if (grid == NULL)
 		return (NULL);
-	
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+			return (NULL);
+	}
+
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
-	
+
 	return (grid);
 }
