2e0d78e5b1480b4b81c901536ef16bc543883d35
Michelle Giraldo <michellegsld@gmail.com>
2019-10-15 16:10:03 -0400

    Third pushing, Adding || for returning NULL, if array == NULL

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *array;
 
-	if (size == 0)
-		return (NULL);
-
 	array = malloc(size * sizeof(char));
+
+	if (size == 0 || array == NULL)
+		return (NULL);
 
 	for (; i <= size; i++)
 		array[i] = c;
