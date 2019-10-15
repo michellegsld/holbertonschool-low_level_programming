7347c9c5cf0a4827baa503645f38c39a7a7ae2a6
Michelle Giraldo <michellegsld@gmail.com>
2019-10-15 16:00:58 -0400

    Initial pushing, Creating an array and initializing to char c, trying to see if missing a NULL check

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * *create_array - Create array of chars, and initialize with specific char.
+ * @size: The size of the array
+ * @c: What the array is initialized with
+ *
+ * Return: Either pointer to array or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(size * sizeof(char));
+
+	for (; i < size; i++)
+		array[i] = c;
+
+	return (array);
+}
