c7b38c2b8db024cb8fd03f82bea8387119d176b8
Michelle Giraldo <michellegsld@gmail.com>
2019-10-16 16:28:43 -0400

    Sixth pushing, changing the variables within the loop checking, if grid[i] == NULL

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			for (j = 0; j < i; j++)
+			for (i = 0; i < width; i++)
 				free(grid[i]);
 			free(grid);
 			return (NULL);
