e103e8090f5bcd7d11e1c790565ddf574b38f54d
Michelle Giraldo <michellegsld@gmail.com>
2019-10-17 14:24:28 -0400

    Fourth pushing, changing what lenb equals, changing if lenb == 0

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,13 +24,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[len2] != '\0')
 		len2++;
 
-	lenb = len1 + len2 + 1;
+	lenb = len1 + n + 1;
 
 	str = malloc(lenb * sizeof(char));
 
 	if (str == NULL)
 		return (NULL);
-	if (lenb == 0)
+
+	if (len1 == 0 && len2 == 0)
 		str = '\0';
 	else
 	{
