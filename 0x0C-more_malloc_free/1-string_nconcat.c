7849b6e530b260c485930cd194a95f8d4799e5b2
Michelle Giraldo <michellegsld@gmail.com>
2019-10-17 14:58:31 -0400

    Sixth pushing, changing len2 value based on n

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,8 +21,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s1[len1] != '\0')
 		len1++;
 
-	while (s2[len2] != '\0' && len2 < n)
+	while (s2[len2] != '\0')
 		len2++;
+
+	if (len2 > n)
+		len2 = n;
 
 	lent = len1 + len2 + 1;
 
