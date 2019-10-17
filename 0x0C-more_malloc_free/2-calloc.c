0d3eb921848e64e8133d50d20cac3a35cb3361bf
Michelle Giraldo <michellegsld@gmail.com>
2019-10-17 15:39:36 -0400

    Second pushing, Including _memset

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
 
+char *_memset(char *s, char b, unsigned int n);
+
 /**
  * *_calloc - Allocate memory for an array.
  * @nmemb: How many elements to be in the array
@@ -9,12 +11,36 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *array;
 
-	array = malloc(nmemb * sizeof(size));
+	void *array;
 
-	if (size == 0 || array == NULL)
+	if (size == 0 || nmemb == 0)
 		return (NULL);
 
+	array = malloc(nmemb * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	_memset(array, size, nmemb);
+
 	return (array);
+}
+
+/**
+ * *_memset - Fills memory with a constant byte.
+ * @s: Pointer
+ * @b: Constant byte
+ * @n: The first bytes of the memory area
+ *
+ * Return: Always the pointer to the memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+		s[i] = b;
+
+	return (s);
 }
