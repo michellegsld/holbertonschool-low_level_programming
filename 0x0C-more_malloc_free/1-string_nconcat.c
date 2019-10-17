99f366336f31284bdbf122ffa10e20668715a8ab
Michelle Giraldo <michellegsld@gmail.com>
2019-10-17 14:00:24 -0400

    Third pushing, Changing for-loops back to initial pushing

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,10 +34,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str = '\0';
 	else
 	{
-		for (; i < len1; i++, j++)
-			str[j] = s1[i];
-		for (i = 0; (i < n) && (i < len2); i++, j++)
-			str[j] = s2[i];
+		for (; i < len1; i++)
+			str[i] = s1[i];
+		for (; (j < n) && (j <= len2); j++)
+			str[i + j] = s2[j];
 	}
 
 	return (str);
