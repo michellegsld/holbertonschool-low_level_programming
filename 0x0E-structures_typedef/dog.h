65f5602751d4d09fc2551f9b570cf0785b625589
Michelle Giraldo <michellegsld@gmail.com>
2019-10-21 14:18:26 -0400

    Second pushing, using betty style, commenting out anything with dog_t, reordering to put structs first

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,30 @@
-#ifndef dog
-#define dog
+#ifndef DOG_H
+#define DOG_H
 
 #include <stdlib.h>
 #include <stdio.h>
 
-int _putchar(char c);
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-void free_dog(dog_t *d);
-
-struct dog {
+/**
+ * struct dog - struct for describing dogs
+ * @name: The name of the dog
+ * @age: How old the dog is
+ * @owner: The owener of the dog's name
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
 
+int _putchar(char c);
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/*
+ * To complete after
+ * dog_t *new_dog(char *name, float age, char *owner);
+ *
+ * void free_dog(dog_t *d);
+*/
 #endif
