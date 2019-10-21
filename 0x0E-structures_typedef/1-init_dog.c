70e982ab3fa1af06b5d4221a8bea84cdfb480514
Michelle Giraldo <michellegsld@gmail.com>
2019-10-21 14:26:13 -0400

    Initial pushing, setting each member of the struct to be corresponding parameter

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - Function initializes a variable of type struct dog
+ * @d: The variable of type struct dog
+ * @name: The name of the dog
+ * @age: How old the dog is
+ * @owner: The dog owner's name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
