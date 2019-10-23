309cdc163660839324db9839138b0252ba166e65
Michelle Giraldo <michellegsld@gmail.com>
2019-10-23 14:14:51 -0400

    Initial pushing, Checking if size is greater than 0, Looping through each element of the array, Using function pointer on each element

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executes a function on each element of an array
+ * @array: Is the array
+ * @size: The size of the array
+ * @action: The pointer to the function needed
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (size > 0)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
