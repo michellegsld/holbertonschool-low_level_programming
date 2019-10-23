85d1ca6a113c0091fa6b73929a6d296a61176a48
Michelle Giraldo <michellegsld@gmail.com>
2019-10-23 19:20:07 -0400

    Initial pushing, May not be correct, Checking which operator was sent in, To get correct function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - Selects the correct function to do the operation
+ * @s: The operator
+ *
+ * Return: The function pointer corresponding to the operator
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while (i < 6 && (s[1] == '\0'))
+	{
+		if (ops[i].op == s)
+			return (ops[i].f);
+		i++;
+	}
+
+	printf("Error\n");
+	exit(99);
+}
