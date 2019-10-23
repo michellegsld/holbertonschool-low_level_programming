49333dbdea6c15e11dc1da2fae9da71165857857
Michelle Giraldo <michellegsld@gmail.com>
2019-10-23 19:42:24 -0400

    Fourth pushing, Deciding to make sure copy provided variables are identical, Changing the 6 to 5 because no longer accounting for the NULL

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,8 +8,6 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	int i = 0;
-
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -18,8 +16,10 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+	int i;
 
-	while (i < 6)
+	i = 0;
+	while (i < 5)
 	{
 		if (ops[i].op == s)
 			return (ops[i].f);
