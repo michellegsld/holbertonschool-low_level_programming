f41dc0b9349fd637060af682aecccf3296a29e6a
Michelle Giraldo <michellegsld@gmail.com>
2019-10-23 19:46:46 -0400

    Fourth pushing, Tried to shorten code a little bit, Still trying to figure out how to use the function pointer

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,9 +11,9 @@ int main(int argc, char *argv[])
 {
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[3]);
-	char *op = argv[2];
-	int (*func)(int, int);
-	int answer;
+	char *optr = argv[2];
+	char op = argv[2][0];
+	int (*func)(int num1, int num2);
 
 	if (argc != 4)
 	{
@@ -21,22 +21,21 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if ((*op == '/' || *op == '%') && num2 == 0)
+	if ((op == '/' || op == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	if (*op != '+' && *op != '-' && *op != '*' && *op != '/'
-			&& *op != '%' && argv[2][1] != '\0')
+	if (op != '+' && op != '-' && op != '*' && op != '/'
+			&& op != '%' && argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	func = (get_op_func(op));
-	answer = (*func)(num1, num2);
+	func = get_op_func(optr);
 
-	printf("%d\n", answer);
+	printf("%d\n", func(num1, num2));
 	return (0);
 }
