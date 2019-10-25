e6d676bf129481af73c87b9f52fc10b583a31e88
Michelle Giraldo <michellegsld@gmail.com>
2019-10-25 16:11:31 -0400

    Initial pushing, Discovered that va_args would increment each time it's used, Decided to create a variable for it, Otherwise very similar to file 1

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Will print strings followed by a newline
+ * @separator: The string to be printed in between the strings
+ * @n: The total number of strings passed
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	va_start(list, n);
+
+	char *arg;
+
+	for (i = 0; i < n; i++)
+	{
+		arg = va_arg(list, char*);
+
+		if (arg)
+			printf("%s", arg);
+		else
+			printf("(nil)");
+
+		if ((i < n - 1) && (separator))
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(list);
+}
