8b7ac92ef3c86c74564f56d39428a653d657105f
Michelle Giraldo <michellegsld@gmail.com>
2019-10-25 14:43:55 -0400

    Second pushing, Accounting for if separator == NULL, Also moving location of newline, Making sure it won't be an else and is after printing all the numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,11 +15,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(list, int));
-		if (i < n - 1)
+		if ((i < n - 1) && separator)
 			printf("%s", separator);
-		else
-			printf("\n");
 	}
+
+	printf("\n");
 
 	va_end(list);
 }
