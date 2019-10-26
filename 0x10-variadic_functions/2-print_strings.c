b399a91fa39c6a74cfffe98e1b7185fdf2701466
Michelle Giraldo <michellegsld@gmail.com>
2019-10-26 15:06:28 -0400

    Second pushing, Moving initialization of *args, Now at the top with the other variables

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,10 +9,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i;
+	char *arg;
 
 	va_start(list, n);
-
-	char *arg;
 
 	for (i = 0; i < n; i++)
 	{
