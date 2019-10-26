87ccc001d89b57ef513420506ed9f40e28b01cfb
Michelle Giraldo <michellegsld@gmail.com>
2019-10-25 20:24:03 -0400

    Initial pushing, Using switch statements for the first time, Could only use 2 while-loops and 2 if statements

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - Will print anything
+ * @format: A list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	char *arg;
+	unsigned int i = 0;
+	int check = 1;
+
+	va_start(list, format);
+
+	while (format != NULL)
+	{
+		while (format[i] != '\0')
+		{
+			check = 1;
+			switch(format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(list, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(list, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(list, double));
+					break;
+				case 's':
+					arg = va_arg(list, char *);
+					if (arg)
+						printf("%s", arg);
+					else
+						printf("(nil)");
+					break;
+				default:
+					check = 0;
+					break;
+			}
+			
+			if ((check == 1) && (format[i + 1] != '\0'))
+				printf(", ");
+			i++;	
+		}
+
+		break;
+	}
+
+	printf("\n");
+
+	va_end(list);
+}
