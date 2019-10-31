b7d4b8c496bcf7ffb6d41de01c8718b698824396
Michelle Giraldo <michellegsld@gmail.com>
2019-10-31 17:19:13 -0400

    3rd Pushing, Printing the result for h->str == NULL directly

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,10 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		total++;
 		h = h->next;
 	}
