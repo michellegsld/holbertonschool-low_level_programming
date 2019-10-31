786cbf9cf7e640acd489d0f44dbb581a5c88d99d
Michelle Giraldo <michellegsld@gmail.com>
2019-10-31 14:03:26 -0400

    1st Pushing, Going through a list, Printing the length of and each string at each node, Returning the amount of nodes

---
+++ 0x12-print_list.c/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all elements of a list_t list
+ * @h: The list
+ *
+ * Return: Always an unsigned int, the total number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int total = 0;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+		total++;
+		h = h->next;
+	}
+
+	while (h)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		total++;
+		h = h->next;
+	}
+
+	return (total);
+}
