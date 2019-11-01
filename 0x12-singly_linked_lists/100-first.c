75b1da708adfb79999dacd00f1fa14e5c2a63345
Michelle Giraldo <michellegsld@gmail.com>
2019-11-01 12:09:17 -0400

    1st Pushing, Function using a constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * before_main - Prints something before main is executed
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
