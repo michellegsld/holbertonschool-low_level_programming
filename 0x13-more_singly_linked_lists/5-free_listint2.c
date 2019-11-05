aa4d14f0cccae1739d3e343c8deb4d1538195fed
Michelle Giraldo <michellegsld@gmail.com>
2019-11-05 14:12:40 -0500

    1st Pushing, Creating a while loop, Going through the list and freeing each node

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a list
+ * @head: The beginning of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	while (*head)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+	}
+}
