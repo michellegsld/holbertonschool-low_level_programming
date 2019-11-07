5c5775681f286c73bd7b03854f8ff1f786c177a4
Michelle Giraldo <michellegsld@gmail.com>
2019-11-07 18:01:56 -0500

    1st Pushing, Creating file as an empty template before-hand

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * set_bit - Sets the value of a bit to 1 at a given position
+ * @n: The number
+ * @index: The given position
+ *
+ * Return: Either 1 or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	return (-1);
+}
