a2cc974187ea55b674bed954c93bc0b587721d88
Michelle Giraldo <michellegsld@gmail.com>
2019-11-07 18:14:02 -0500

    1st Pushing, Creating an empty file as an outline

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,10 +4,12 @@
  * flip_bits - Finds how many bits needed to flip between two numbers
  * @n: The first number
  * @m: The second number
+ *
+ * Return: Always the number of bits needed to flip
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int total = 0;
 
-	return (0);
+	return (total);
 }
