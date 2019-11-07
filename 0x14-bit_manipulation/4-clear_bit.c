39280ad7972b4a5ecc387dedc7db7985378d5f09
Michelle Giraldo <michellegsld@gmail.com>
2019-11-07 18:13:00 -0500

    1st Pushing, Creating an empty file as an outline

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,6 +4,8 @@
  * clear_bit  - Sets the value of a bit to 0 at a given index
  * @n: The number
  * @index: The given position of where to change the bit
+ *
+ * Return: Either 1 or -1
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
