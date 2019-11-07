c405b36d15ec4a02e900090ec5baa09bf39ef95b
Michelle Giraldo <michellegsld@gmail.com>
2019-11-07 18:08:36 -0500

    1st Pushing, Creating an empty file as an outline

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - Finds how many bits needed to flip between two numbers
+ * @n: The first number
+ * @m: The second number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int total = 0;
+
+	return (0);
+}
