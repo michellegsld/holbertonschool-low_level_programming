d38829623836015ef1e3205b7864cdabd0aa435b
Michelle Giraldo <michellegsld@gmail.com>
2019-11-07 18:04:42 -0500

    1st Pushing, Creating file as an empty template before-hand

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * clear_bit  - Sets the value of a bit to 0 at a given index
+ * @n: The number
+ * @index: The given position of where to change the bit
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	return (-1);
+}
