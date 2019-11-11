e74ca2a01f9b0f9e16760312134ef04d0a48ccfd
Michelle Giraldo <michellegsld@gmail.com>
2019-11-11 15:22:54 -0500

    3rd Pushing, Creating an extra variable, Creative prev1 and prev2, Adding both together to get the sum, No longer adding sum to sum

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,27 +3,27 @@
 /**
  * main - Prints the first 50 Fibonacci numbers.
  *
- * Starts with 1 and 2, followed by a newline..
+ * Starts with 1 and 2, followed by a newline.
  * Return: Always 0.
  */
 int main(void)
 {
 	int counter = 0;
-	long sum = 1, prev = 0;
+	long sum = 0, prev1 = 0, prev2 = 1;
 
 	for (; counter < 50; counter++)
 	{
-		prev = sum;
-
-		if (counter > 0)
-			sum = prev + sum;
+		sum = prev1 + prev2;
 
 		printf("%ld", sum);
-		
+
 		if (counter < 49)
 			printf(", ");
 		else
 			printf("\n");
+
+		prev1 = prev2;
+		prev2 = sum;
 	}
 	return (0);
 }
