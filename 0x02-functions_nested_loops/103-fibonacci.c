0fcc28c9a8fe9ed660d807e53f58c7c97d081739
Michelle Giraldo <michellegsld@gmail.com>
2019-11-11 15:32:42 -0500

    1st Pushing, Creating while loop, Moving the comma and space to print before the number, Checking if the sum is even before doing so

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints even Fibonacci numbers below 4 million
+ *
+ * Starts with 1 and 2, followed by a newline.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long sum = 0, prev1 = 0, prev2 = 1;
+
+	while ((prev1 + prev2) < 4000000)
+	{
+		sum = prev1 + prev2;
+
+		if (sum % 2 == 0)
+		{
+			if (sum > 2)
+				printf(", ");
+			printf("%ld", sum);
+		}
+
+		prev1 = prev2;
+		prev2 = sum;
+	}
+
+	printf("\n");
+
+	return (0);
+}
