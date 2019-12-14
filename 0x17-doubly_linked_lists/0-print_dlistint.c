8914cdc06a21d383c178d29fc05d3788f9111cff
Michelle Giraldo <michellegsld@gmail.com>
2019-12-14 14:05:21 -0500

    1st Pushing, Printing each element in a list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints every node of a list
+ * @h: The head of the list
+ *
+ * Return: The number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count;
+
+	while(h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return(count);
+}
