41b16c5954e1eedc4c5abbf9919cb5ff4d74c7f0
Michelle Giraldo <michellegsld@gmail.com>
2020-04-15 17:49:26 -0400

    [Update] Change way print statements work

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -21,7 +21,10 @@ int binary_search(int *array, size_t size, int value)
 
 		if (array[middle] == value)
 		{
-			print_array(array, middle, end);
+			if (middle < end)
+				print_array(array, middle, end);
+			else
+				printf("Searching in array: %d\n", array[middle]);
 			return (middle);
 		}
 
