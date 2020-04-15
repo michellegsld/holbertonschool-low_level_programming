75b47ba54bd3a987fa5f7f1985155f49001b5834
Michelle Giraldo <michellegsld@gmail.com>
2020-04-15 17:38:17 -0400

    [Initial] Use helper print function and loop through array with a while loop for binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,55 @@
+#include "search_algos.h"
+
+void print_array(int *array, size_t start, size_t end);
+
+/**
+ * binary_search - Searches for a value in a sorted array of integers
+ * @array: A pointer to the first element of the array
+ * @size: Number of elements in array
+ * @value: The value to search for
+ *
+ * Return: -1 or index where value is located
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t start = 0, end = size - 1, middle = end - start;
+
+	while (start <= end)
+	{
+		print_array(array, start, end);
+		middle = start + middle / 2;
+
+		if (array[middle] == value)
+		{
+			print_array(array, middle, end);
+			return (middle);
+		}
+
+		if (array[middle] < value)
+			start = middle + 1;
+		else
+			end = middle - 1;
+	}
+	print_array(array, start, end);
+	return (-1);
+}
+
+/**
+ * print_array - Helper function to print array
+ * @array: The array to print
+ * @start: Where to start printing in the array
+ * @end: Last element to print in array
+ */
+void print_array(int *array, size_t start, size_t end)
+{
+	size_t idx = start;
+
+	printf("Searching in array: ");
+	for (; start <= end && array[idx] != array[end]; idx++)
+	{
+		printf("%d", array[idx]);
+		printf(", ");
+	}
+
+	printf("%d\n", array[end]);
+}
